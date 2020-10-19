edf2df56cad5885db3ec79f977be28ada2d7205b
Thrace123 <72183930+Thrace123@users.noreply.github.com>
2020-10-19 18:20:38 +0000

    1

---
+++ src/exercise3/NumberCounter.java
@@ -0,0 +1,42 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class NumberCounter {
+  public static void main (String[] args) {
+   Scanner input = new Scanner(System.in);
+
+
+  int posCount = 0;
+int negCount= 0;
+ double sum = 0.0;
+double averrage = 0.0;
+int count=0;
+  int number = input.nextInt();
+
+while ( number != 0) {
+    if (number > 0){
+      posCount++;
+    }else{
+      negCount++;
+      }
+
+  
+    sum += number;
+    
+    number=input.nextInt();
+  
+   
+  }
+
+  averrage = (sum)/count;
+
+  System.out.println("Positive numbers: "+ posCount);
+  System.out.println("Negative numbers: "+ negCount);
+  System.out.println("Sum: " + sum);
+  System.out.println("Average: " + averrage);
+  
+
+
+  }
+}
