e7e134bb7cea496c13500500ceb9995ba3401066
Thrace123 <72183930+Thrace123@users.noreply.github.com>
2020-10-19 20:06:13 +0000

    3

---
+++ src/exercise3/NumberSearch.java
@@ -0,0 +1,23 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class NumberSearch {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+       int Count=0;
+       if (index%5==0 && index%6==0){
+      count++;
+
+   if (count==10){
+      count=0;
+      System.out.print(index);
+       System.out.println();
+   }else {
+     System.out.print(index +" ");
+   }
+       }
+    }
+}
